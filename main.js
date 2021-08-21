5a6fe998d8f10e8af58962f62d19aa9ac899ddad
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2021-08-21 12:48:54 -0700

    Project 2 initial commit
    Added main.js  and linked to index. Added pseudo-code and  some logic to only run the app when a link on index is clicked.

---
+++ main.js
@@ -0,0 +1,36 @@
+//Car Comparer
+
+//App will use user-selected normal car and compare to user-selected ferrari
+
+//Alert user - Explain app 
+
+//display list of normal cars 
+
+//ask user to pick one
+
+//display list of ferrari
+
+//ask user to pick one
+
+//function to get info from collection for each car and arrange in legible way
+
+//display the comparioson to user
+
+//end
+
+//testing code
+        // Wait for the page to load first
+        window.onload = function() {
+
+            //Get a reference to the link on the page
+            // with an id of "mylink"
+            var a = document.getElementById("compareL");
+  
+            //Set code to run when the link is clicked
+            // by assigning a function to "onclick"
+            a.onclick = function() {
+                alert("Test");
+              
+              return false;
+            }
+          }
