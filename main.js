ec12918e78b3c1d42120f8cc28a7aab63dd6c09c
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2021-08-21 14:03:35 -0700

    update to main.js
    Added arrays to hold the car information. Populated the arrays.

--- main.js
+++ main.js
@@ -18,19 +18,34 @@
 
 //end
 
-//testing code
-        // Wait for the page to load first
-        window.onload = function() {
+//code
 
-            //Get a reference to the link on the page
-            // with an id of "mylink"
-            var a = document.getElementById("compareL");
-  
-            //Set code to run when the link is clicked
-            // by assigning a function to "onclick"
-            a.onclick = function() {
-                alert("Test");
-              
-              return false;
-            }
-          }
+//cars - using arrays for now
+//[name, price, engine, hp, torque, mpg, topspeed, 0-60]
+//["","","","","","","",""]
+//normal
+const corolla = ["2021 Toyota Corolla","From $20,025","2.0L I4","169 hp","126 lb-ft","31 city / 40 highway","118 mph","8.2 sec"]
+const accord = ["2021 Honda Accord", "From $24,970","2.0L Turbocharged I4","252 hp","273 lb-ft","30 city / 38 highway","115 mph","7.2 sec"]
+const mustang = ["2021 Ford Mustang GT","From $36,285","5.0L Ti-VCT V-8","460 hp","420 lb-ft","15 city / 24 highway","155 mph","4.4 sec"]
+
+//ferrari
+const italia = ["2015 Ferrari 458 Italia","From $245,000","4.5L V8","562 hp","398 lb-ft","13 city / 17 highway","202 mph","3.4 sec"]
+const enzo = ["2003 Ferrari Enzo","Was $643,330 / Now $2,530,000","6.0L DOHC V12","651 hp","485 lb-ft","8 city / 12 highway","218 mph","3.3 sec"]
+const gtb = ["2022 Ferrari 296 GTB","$321,000","3.0L Twin-Turbo V6/165hp Electric","819 hp","546 lb-ft","Unknown","205 mph","2.9 sec"]
+
+//The script should only run if the link is clicked.
+// Wait for the page to load first
+window.onload = function() {
+
+    //Get a reference to the link on the page
+    // with an id of "compareL"
+    var a = document.getElementById("compareL");
+
+    //Set code to run when the link is clicked
+    // by assigning a function to "onclick"
+    a.onclick = function() {
+        alert("This tool will let you see the difference between modern normal cars and a Ferrari!");
+        
+        return false;
+    }
+}
